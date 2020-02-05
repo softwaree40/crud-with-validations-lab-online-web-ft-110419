7436afd68e65bab0f41a26a831a988fd56c924d7
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-05 22:48:37 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,9 @@
 class Song < ApplicationRecord
-    validates  :title, presence: true 
+    validates  :title, presence: true
     validates  :title, uniqueness: true
     validates  :released, presence: true
-    validates  :artist_name, presence: true 
+    validates  :artist_name, presence: true
+    validates  :released, presence: true, if: :released
+    validates  :release_year, numericality: { only_integer: true , less_than_or_equal_to: Time.now.year}, if: :released
     
 end
