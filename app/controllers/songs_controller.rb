cc074af5c25b49be985c45562824bdd3557f0bbb
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-06 05:23:49 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,7 @@ class SongsController < ApplicationController
     def update
        @song = Song.find_by(id: params[:id])
        @song.update(song_params)
-       redirect_to edit_song_path(song)
+       redirect_to edit_song_path(@song)
     end
     
     def destroy 
